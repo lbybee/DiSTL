7566ef225d7f2dd8753b39c70e690403a8c0c430
lbybee <leland.bybee@gmail.com>
2018-09-07 16:07:58 -0400

    add query methods

---
+++ DiSTL/query/DJ_methods.py
@@ -0,0 +1,404 @@
+def gen_tag_sql(date, tag_drop_dict):
+    """generates query to select documents based on tag drop lists
+
+    Parameters
+    ----------
+    date : str
+        date for corresponding tables
+    tag_drop_dict : dict-like
+        dictionary containing tag categories to drop (keys) as well as list
+        of actual tags to drop (values)
+
+    Returns
+    -------
+    tag sql query
+    """
+
+    # select doc_ids from tag link tables
+    tag_link_sql = "SELECT doc_id FROM"
+    sql_l = []
+    for tag_cat in tag_drop_dict:
+        sql = """SELECT doc_id
+                    FROM tag_link_%s_%s
+                    INNER JOIN
+                    (SELECT tag_id
+                     FROM tag_%s
+                     WHERE tag_label IN (%s)) tmp_tag
+                    ON tag_link_%s_%s.tag_id
+                       = tmp_tag.tag_id
+          """ % (tag_cat, date, tag_cat,
+                 ",".join(["'%s'" % t for t in tag_drop_dict[tag_cat]]),
+                 tag_cat, date)
+        sql_l.append(sql)
+    tag_link_sql += " (%s) AS foo" % " UNION ".join(sql_l)
+
+    # join with doc table
+    tag_sql = """SELECT *
+                    FROM doc_%s l
+                    WHERE NOT EXISTS
+                    (SELECT doc_id
+                     FROM (%s) r
+                     WHERE r.doc_id = l.doc_id
+                    )
+              """ % (date, tag_link_sql)
+
+    return tag_sql
+
+
+def gen_doc_sql(tag_sql, date, headline_l=[], author_l=[],
+                article_threshold=0):
+    """
+    generates query to select documents based on doc specifics
+
+    This does the following:
+
+    1. remove weekends
+    2. drop documents matching headline start patterns
+    3. drop documents containing author patterns
+    4. drop documents below term_count threshold
+
+    Parameters
+    ----------
+    tag_sql : str
+        query returned by gen_tag_sql
+    date : str
+        date label for current table
+    headline_l : list
+        list of headline patterns to drop
+    author_l : list
+        list of author patterns to drop
+    article_threshold : int
+        headline + body term count required to keep article
+
+    Returns
+    -------
+    doc sql query
+    """
+
+    doc_sql = """SELECT *
+                    FROM (%s) AS foo
+                    WHERE EXTRACT(DOW FROM display_date) NOT IN (6, 0)
+                    AND headline !~* %s
+                    AND headline !~* %s
+                    AND (headline_term_count + body_term_count) > %d
+              """ % (tag_sql,
+                     "'^(" + "|".join(headline_l) + ").*'",
+                     "'.*(" + "|".join(author_l) + ").*'",
+                     article_threshold)
+
+    return doc_sql
+
+
+def gen_company_sql(doc_sql, date, company_id_l):
+    """joins the desired company ids with the doc sql
+
+    Parameters
+    ----------
+    doc_sql : str
+        query returned by gen_doc_sql
+    date : str
+        date label
+    company_id_l : list of str
+        list of tickers to extract
+
+    Returns
+    -------
+    updated query
+    """
+
+    sql = """SELECT tag_label,
+                    tmp_doc.*
+             FROM (SELECT tag_label, tag_link_djn_company_%s.tag_id, doc_id
+                   FROM
+                   (SELECT tag_label, tag_id
+                    FROM tag_djn_company
+                    WHERE tag_label IN (%s)
+                   ) AS tmp_tag
+                   INNER JOIN tag_link_djn_company_%s
+                   ON tmp_tag.tag_id = tag_link_djn_company_%s.tag_id
+                  ) AS tmp_tag_doc
+             INNER JOIN (%s) AS tmp_doc
+             ON tmp_tag_doc.doc_id = tmp_doc.doc_id
+          """ % (date, ",".join(["'%s'" % t for t in company_id_l]),
+                 date, date, doc_sql)
+
+    return sql
+
+
+def gen_article_doc_sql(doc_sql, threshold=0):
+    """generates the query for extracting articles from the database
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+    threshold : int
+        term count threhsold for dropping further articles
+
+    Returns
+    -------
+    doc query
+    """
+
+    sql = """SELECT doc_id,
+                    display_date,
+                    accession_number,
+                    dense_rank() OVER(ORDER BY doc_id) AS new_doc_id
+                FROM (%s) AS tmp_doc
+                WHERE (headline_term_count + body_term_count) > %d
+            """ % (doc_sql, threshold)
+    return sql
+
+
+def gen_article_doc_id_sql(doc_sql):
+    """generate the query needed to write the article doc id info to a csv
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+
+    Returns
+    -------
+    doc id query
+    """
+
+    sql = """SELECT new_doc_id, display_date, accession_number
+                FROM (%s) as tmp_doc
+          """ % doc_sql
+    return sql
+
+
+def gen_ticker_day_doc_sql(doc_sql, threshold=0):
+    """generates the query for extracting ticker-days from the database
+    (applying any daily threhsolding in the process)
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+    threshold : int
+        term count threhsold for dropping ticker-days
+
+    Returns
+    -------
+    doc query
+    """
+
+    # generate the new id for thresholding
+    # TODO this could likely be simplified
+    sql = """SELECT tag_label,
+                    date(display_date) AS day_date,
+                    tmp_doc.doc_id,
+                    (tmp_doc.headline_term_count
+                     + tmp_doc.body_term_count) AS term_count,
+                    dense_rank()
+                    OVER(ORDER BY tag_label,
+                        date(display_date))
+                    AS new_doc_id
+             FROM (%s) AS tmp_doc
+          """ % (doc_sql)
+
+    # select based on daily threshold
+    sql = """SELECT tag_label,
+                    day_date,
+                    doc_id,
+                    main_doc.new_doc_id
+             FROM (%s) AS main_doc
+             LEFT JOIN (SELECT new_doc_id
+                        FROM
+                        (SELECT new_doc_id,
+                                sum(term_count) AS term_count
+                         FROM (%s) AS foo
+                         GROUP BY new_doc_id
+                        ) AS foobar
+                        WHERE term_count > %d
+                       ) AS tmp_doc
+            ON main_doc.new_doc_id = tmp_doc.new_doc_id
+          """ % (sql, sql, threshold)
+
+    return sql
+
+
+def gen_ticker_day_doc_id_sql(doc_sql):
+    """generate the query needed to write the ticker-day doc id info to a csv
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+
+    Returns
+    -------
+    doc id query
+    """
+
+    sql = """SELECT DISTINCT ON (new_doc_id)
+                tag_label, day_date, new_doc_id
+                FROM (%s) as tmp_doc""" % doc_sql
+    return sql
+
+def gen_ticker_month_doc_sql(doc_sql, threshold=0):
+    """generates the query for extracting ticker-months from the database
+    (applying any monthly threhsolding in the process)
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+    threshold : int
+        term count threhsold for dropping ticker-months
+
+    Returns
+    -------
+    doc query
+    """
+
+    # generate the new id for thresholding
+    # TODO this could likely be simplified
+    sql = """SELECT tag_label,
+                    extract(year FROM display_date) AS year,
+                    extract(month FROM display_date) AS month,
+                    tmp_doc.doc_id,
+                    (tmp_doc.headline_term_count
+                     + tmp_doc.body_term_count) AS term_count,
+                    dense_rank()
+                    OVER(ORDER BY tag_label,
+                        extract(year FROM display_date),
+                        extract(month FROM display_date))
+                    AS new_doc_id
+             FROM (%s) AS tmp_doc
+          """ % (doc_sql)
+
+    # select based on daily threshold
+    sql = """SELECT tag_label,
+                    year,
+                    month,
+                    doc_id,
+                    main_doc.new_doc_id
+             FROM (%s) AS main_doc
+             LEFT JOIN (SELECT new_doc_id
+                        FROM
+                        (SELECT new_doc_id,
+                                sum(term_count) AS term_count
+                         FROM (%s) AS foo
+                         GROUP BY new_doc_id
+                        ) AS foobar
+                        WHERE term_count > %d
+                       ) AS tmp_doc
+            ON main_doc.new_doc_id = tmp_doc.new_doc_id
+          """ % (sql, sql, threshold)
+
+    return sql
+
+
+def gen_ticker_month_doc_id_sql(doc_sql):
+    """generate the query needed to write the ticker-month doc id info to a
+    csv
+
+    Parameters
+    ----------
+    doc_sql : str
+        current sql query
+
+    Returns
+    -------
+    doc id query
+    """
+
+    sql = """SELECT DISTINCT ON (new_doc_id)
+                tag_label, year, month, new_doc_id
+                FROM (%s) as tmp_doc""" % doc_sql
+    return sql
+
+
+def gen_count_query(doc_sql, date, t_label, ngram):
+    """joins the temporary term and doc tables with the counts and
+    dumps the results to the provided file
+
+    Parameters
+    ----------
+    sql : str
+        current sql query
+    date : str
+        date label
+    t_label : str
+        label for current txt category
+    ngram : str
+        ngram label
+
+    Returns
+    -------
+    update sql for writing counts
+    """
+
+    sql = """SELECT tmp_doc.new_doc_id, tmp_term_%s.new_term_id, sum(count)
+                FROM count_%s_%s_%s
+                INNER JOIN (%s) AS tmp_doc
+                ON count_%s_%s_%s.doc_id = tmp_doc.doc_id
+                INNER JOIN tmp_term_%s
+                ON count_%s_%s_%s.term_id = tmp_term_%s.term_id
+                GROUP BY tmp_doc.new_doc_id, tmp_term_%s.new_term_id
+            """ % (ngram, t_label, ngram, date, doc_sql, t_label, ngram,
+                   date, ngram, t_label, ngram, date, ngram, ngram)
+
+    return sql
+
+
+def gen_doc_query(date, tag_drop_dict={}, headline_l=[], author_l=[],
+                  article_threshold=0, company_id_l=[], agg_type="article",
+                  agg_threshold=0):
+    """generates the doc query and doc id query for the provided params
+
+    Parameters
+    ----------
+    date : str
+        date for corresponding tables
+    tag_drop_dict : dict-like
+        dictionary containing tag categories to drop (keys) as well as list
+        of actual tags to drop (values)
+    headline_l : list
+        list of headline patterns to drop
+    author_l : list
+        list of author patterns to drop
+    article_threshold : int
+        headline + body term count required to keep article
+    company_id_l : list of str
+        list of tickers to extract
+    agg_type : str
+        lvl of aggregation for final document
+    agg_threshold : int
+        any additional thresholding done at final aggregate lvl
+
+    Returns
+    -------
+    tuple containing doc threshold and doc id threhsold
+    """
+
+    if len(tag_drop_dict) > 0:
+        sql = gen_tag_sql(date, tag_drop_dict)
+    else:
+        sql = "SELECT * FROM doc_%s" % date
+
+    if len(headline_l) > 0 or len(author_l) > 0 or article_threshold > 0:
+        sql = gen_doc_sql(sql, date, headline_l, author_l, article_threshold)
+
+    if len(company_id_l) > 0:
+        sql = gen_company_sql(sql, date, company_id_l)
+
+    if agg_type == "article":
+        doc_sql = gen_article_doc_sql(sql, agg_threshold)
+        doc_id_sql = gen_article_doc_id_sql(doc_sql)
+
+    elif agg_type == "ticker_day":
+        doc_sql = gen_ticker_day_doc_sql(sql, agg_threshold)
+        doc_id_sql = gen_ticker_day_doc_id_sql(doc_sql)
+
+    elif agg_type == "ticker_month":
+        doc_sql = gen_ticker_month_doc_sql(sql, agg_threshold)
+        doc_id_sql = gen_ticker_month_doc_id_sql(doc_sql)
+
+    else:
+        raise ValueError("unsupported agg_type: %s" % agg_type)
+
+    return doc_sql, doc_id_sql
---
+++ DiSTL/query/global_methods.py
@@ -0,0 +1,254 @@
+from datetime import datetime
+import multiprocessing
+import psycopg2
+import os
+
+
+def create_temp_term_table(db, schema, ngram, threshold, stop_words,
+                           regex_stop_words, data_dir, log_file):
+    """generates a table containing the term_label and term_id for the
+    ngrams kept after applying threshold, stop_words and regex_stop_words
+    filters
+
+    Note that we can't use a temporary table here because we need this table
+    to exist across sessions so that the count building can happen in
+    parallel
+
+    Parameters
+    ----------
+    db : str
+        database name
+    schema : str
+        schema name
+    ngram : str
+        label for ngram e.g. 1gram, 2gram
+    threshold : int
+        count below which we drop terms
+    stop_words : list
+        list of stop words
+    regex_stop_words : list
+        list of regex stop words
+    data_dir : str
+        location where output file will be stored
+    log_file : str
+        location where log is written when done
+
+    Returns
+    -------
+    None
+    """
+
+    # establish connection
+    t0 = datetime.now()
+    conn = psycopg2.connect(host="localhost", database=db,
+                            user="postgres", password="postgres")
+    cursor = conn.cursor()
+    cursor.execute("SET search_path TO %s" % schema)
+    conn.commit()
+
+    sql = """CREATE TABLE tmp_term_%s AS
+                SELECT term_label, term_id,
+                       dense_rank() OVER(ORDER BY term_id) AS new_term_id
+                FROM term_%s
+                WHERE doc_count > %d
+                AND term_label NOT IN (%s)
+                AND term_label !~ %s
+          """ % (ngram, ngram, threshold,
+                 ",".join(["'%s'" % w for w in stop_words]),
+                 "'.*(" + "|".join(regex_stop_words) + ").*'")
+
+    cursor.execute(sql)
+    conn.commit()
+
+    copy_sql = "(SELECT term_label, new_term_id FROM tmp_term_%s)" % ngram
+
+    # now dump table to csv
+    with open(os.path.join(data_dir, "term_id_%s.csv" % ngram), "w") as fd:
+        cursor.copy_to(fd, copy_sql, sep=",")
+
+    t1 = datetime.now()
+    with open(log_file, "a") as fd:
+        fd.write("term,%s,%s,%s\n" % (ngram, str(t1), str(t1 - t0)))
+
+    cursor.close()
+    conn.close()
+
+
+def drop_temp_term_table(db, schema, ngram):
+    """drops all the temp term tables
+
+    Parameters
+    ----------
+    db : str
+        database name
+    schema : str
+        schema name
+    ngram : str
+        label of ngram to drop
+
+    Returns
+    -------
+    None
+    """
+
+    # establish connection
+    conn = psycopg2.connect(host="localhost", database=db,
+                            user="postgres", password="postgres")
+    cursor = conn.cursor()
+    cursor.execute("SET search_path TO %s" % schema)
+    conn.commit()
+
+    sql = "DROP TABLE tmp_term_%s;" % ngram
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def count_query(db, schema, date, txt_labels, ngram_l, gen_doc_query,
+                gen_count_query, data_dir, log_file, gen_doc_query_kwds=None):
+    """selects the counts which meet the search criteria for terms and docs
+    and writes them to a set of output files
+
+    Parameters
+    ----------
+    db : str
+        name of database
+    schema : str
+        name of schema
+    date : str
+        date label
+    txt_labels : list
+        list of labels for txt categories
+    ngram_l : list
+        list of ngram labels
+    gen_doc_query : function
+        method for generating doc queries
+    gen_count_query : function
+        method for generating count queries
+    data_dir : str
+        location where output is to be stored
+    log_file : None or str
+        if None location where log is written when done
+    gen_doc_query_kwds : dict-like or None
+        key words passed to gen_doc_query
+
+    Returns
+    -------
+    None
+
+    Writes
+    ------
+    1. doc_id files
+    2. count files
+    """
+
+    if gen_doc_query_kwds is None:
+        gen_doc_query_kwds = {}
+
+    t0 = datetime.now()
+    conn = psycopg2.connect(host="localhost", database=db,
+                            user="postgres", password="postgres")
+    cursor = conn.cursor()
+    cursor.execute("SET search_path TO %s" % schema)
+    conn.commit()
+
+    # prep doc sql and doc_id_sql
+    doc_sql, doc_id_sql = gen_doc_query(date, **gen_doc_query_kwds)
+
+    # write doc id
+    with open(os.path.join(data_dir, "doc_id_%s.csv" % date), "w") as fd:
+        cursor.copy_to(fd, "(%s)" % doc_id_sql, sep=",")
+
+    t1 = datetime.now()
+    with open(log_file, "a") as fd:
+        fd.write("doc,%s,%s,%s\n" % (date, str(t1), str(t1 - t0)))
+
+    # for each ngram and term label extract counts
+    for ngram in ngram_l:
+        for t_label in txt_labels:
+            t0 = datetime.now()
+            count_sql = gen_count_sql(doc_sql, t_label, ngram, date)
+
+            count_f = os.path.join(data_dir, "count_%s_%s_%s.csv" % (t_label,
+                                                                     ngram,
+                                                                     date))
+            with open(count_f, "w") as fd:
+                cursor.copy_to(fd, "(%s)" % count_sql, sep=",")
+
+            t1 = datetime.now()
+            with open(log_file, "a") as fd:
+                fd.write("count,%s,%s,%s,%s,%s\n" % (t_label, ngram, date,
+                                                     str(t1), str(t1 - t0)))
+
+
+def DTM_wrapper(db, schema, processes, dates, txt_labels, ngram_l,
+                gen_doc_query, gen_count_query, data_dir, log_file,
+                ngram_term_threshold_dict, ngram_stop_words_dict,
+                ngram_regex_stop_words_dict, gen_doc_query_kwds=None):
+    """wrapper called by run script to build DTM
+
+    Parameters
+    ----------
+    db : str
+        name of database
+    schema : str
+        name of schema
+    processes : int
+        number of processes for multiprocessing pool
+    dates : list
+        list of date label
+    txt_labels : list
+        list of labels for txt categories
+    ngram_l : list
+        list of ngram labels
+    gen_doc_query : function
+        method for generating doc queries
+    gen_count_query : function
+        method for generating count queries
+    data_dir : str
+        location where output is to be stored
+    log_file : None or str
+        if None location where log is written when done
+    ngram_term_threshold_dict : dict-like
+        dictionary mapping each ngram to a term threshold
+    ngram_stop_words_dict : dict-like
+        dictionary mapping each ngram to stop words list
+    ngram_regex_stop_words_dict : dict-like
+        dictionary mapping each ngram to regex stop words list
+    gen_doc_query_kwds : dict-like or None
+        key words passed to gen_doc_query
+
+    Returns
+    -------
+    None
+
+    Writes
+    ------
+    1. doc_id files
+    2. count files
+    3. term files
+    """
+
+    open(log_file, "w")
+
+    # initialize multiprocessing
+    pool = multiprocessing.Pool(processes)
+
+    # create tmp term tables and save
+    pool.starmap(create_temp_term_table, [(db, schema, ngram,
+                                           ngram_term_threshold_dict[ngram],
+                                           ngram_stop_words_dict[ngram],
+                                           ngram_regex_stop_words_dict[ngram],
+                                           data_dir, log_file)
+                                          for ngram in ngram_l])
+
+    # count (and doc) query
+    pool.starmap(count_query, [(db, schema, d, txt_labels, ngram_l,
+                                gen_doc_query, gen_count_query, data_dir,
+                                log_file, gen_doc_query_kwds)
+                               for d in dates])
+
+    # drop tmp tables
+    pool.starmap(drop_temp_term_table, [(db, schema, ngram) for ngram in
+                                        ngram_l])
